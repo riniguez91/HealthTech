bb8d535d174ef559f6ed1f0326ade0bd4ee54b5a
victor00hs <46093788+victor00hs@users.noreply.github.com>
2020-01-07 19:02:30 +0100

    Revert "Create SensorGas.json y encriptarMD5 movido modelo"
    This reverts commit 127913a08c1a4887e2f160585ffc0fda03c6dafa.

--- Proyecto1/src/application/controladores/controladorLogin.java
+++ Proyecto1/src/application/controladores/controladorLogin.java
@@ -332,4 +332,27 @@ public class controladorLogin {
         slideshow.play();
     }
     
+    
+    // Encriptacion de las constraseñas en MD5
+    private static final char[] CONSTS_HEX = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+    
+    public static String encriptaEnMD5(String stringAEncriptar) {
+        try {
+           MessageDigest msgd = MessageDigest.getInstance("MD5");
+           byte[] bytes = msgd.digest(stringAEncriptar.getBytes());
+           StringBuilder strbCadenaMD5 = new StringBuilder(2 * bytes.length);
+           for (int i = 0; i < bytes.length; i++) {
+               int bajo = (int)(bytes[i] & 0x0f);
+               int alto = (int)((bytes[i] & 0xf0) >> 4);
+               strbCadenaMD5.append(CONSTS_HEX[alto]);
+               strbCadenaMD5.append(CONSTS_HEX[bajo]);
+           }
+           return strbCadenaMD5.toString();
+        } catch (NoSuchAlgorithmException e) {
+           return null;
+        }   
+    }
+    
+ 
+    
 }
--- Proyecto1/src/application/modelos/modelo.java
+++ Proyecto1/src/application/modelos/modelo.java
@@ -7,8 +7,6 @@ import javafx.scene.control.Alert;
 
 import java.io.*;
 import java.lang.reflect.Type;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -154,29 +152,7 @@ public class modelo {
         alert.showAndWait();
     }
     
-    
-    // Encriptacion de las constraseñas en MD5
-    private static final char[] CONSTS_HEX = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
-    
-    public static String encriptaEnMD5(String stringAEncriptar) {
-        try {
-           MessageDigest msgd = MessageDigest.getInstance("MD5");
-           byte[] bytes = msgd.digest(stringAEncriptar.getBytes());
-           StringBuilder strbCadenaMD5 = new StringBuilder(2 * bytes.length);
-           for (int i = 0; i < bytes.length; i++) {
-               int bajo = (int)(bytes[i] & 0x0f);
-               int alto = (int)((bytes[i] & 0xf0) >> 4);
-               strbCadenaMD5.append(CONSTS_HEX[alto]);
-               strbCadenaMD5.append(CONSTS_HEX[bajo]);
-           }
-           return strbCadenaMD5.toString();
-        } 
-        catch (NoSuchAlgorithmException e) {
-           return null;
-        }   
-    }
-    
-    // Validador de DNI 
+ // Validador de DNI    
     public boolean validarDNI(String dni) { 
         boolean esValido = false;
         int i = 0;
